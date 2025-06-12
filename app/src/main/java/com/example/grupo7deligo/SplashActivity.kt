4c761c41a65776407270da70908bb44d794a7296
LuzAndrade7 <luzmaria.andrade@epn.edu.ec>
2025-06-11 23:08:50 -0500

    Deligo - SplashActivity

---
+++ app/src/main/java/com/example/grupo7deligo/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.grupo7deligo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Redirige automáticamente a PhoneNumberActivity después de 2 segundos
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, PhoneNumberActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
